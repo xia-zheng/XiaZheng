6f83715580b73dcd24d3666c103a1e772e8e1a5e
Xiazheng <xiazheng.hhu@qq.com>
2023-05-10 14:50:41 +0800

    121

--- ParameterManagerDemo/mainwindow.h
+++ ParameterManagerDemo/mainwindow.h
@@ -1,7 +1,7 @@
 /*
  * @Author       : XiaZheng <xiazheng.hhu@qq.com>
  * @Date         : 2023-05-01 21:11:33
- * @LastEditTime : 2023-05-10 14:49:21
+ * @LastEditTime : 2023-05-10 14:50:32
  * @FilePath     : \XiaZheng\ParameterManagerDemo\mainwindow.h
  * @Description  : 
  */
@@ -26,3 +26,4 @@ private:
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
+111
